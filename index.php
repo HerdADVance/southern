202603ba44ca15320d10ab713b8945d0fb445224
Alex Vance <herdadvance@gmail.com>
2019-02-27 16:26:34 -0500

    Almost done with homepage

---
+++ index.php
@@ -0,0 +1,190 @@
+<!doctype html>
+<html lang="en">
+  <head>
+
+    <meta charset="UTF-8">
+    <title>Southern West Virginia Community and Technical College</title>
+    <link rel="icon" href="favicon.ico">
+
+    <meta name="author" content="Bulldog Creative Services">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" />
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-9ralMzdK1QYsk4yBY680hmsb4/hJ98xK3w0TIaJ3ll4POWpWUYaA2bRjGGujGT8w" crossorigin="anonymous">
+    <link href="/assets/css/normalize.css" rel="stylesheet">
+    <link href="/assets/css/style.css" rel="stylesheet">
+    <link href="/assets/css/mobile.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,300,400,600,700" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+
+  </head>
+
+  <body class="home">
+
+  	<header class="header">
+        <div class="header-top pri row unflex">
+            <div class="col-6">
+                <ul class="fa-ul ul-inline no-mar">
+                    <li class="top-phone"><a href="tel:1-304-896-7474"><span class="fa-li"></span><i class="fas fa-phone"></i>304.896.7474</a></li>
+                    <li class="top-email"><a href="mailto:admissions@southernwv.edu"><span class="fa-li"></span><i class="fal fa-envelope"></i>admissions@southernwv.edu</a></li>
+                </ul>
+            </div>
+            <div class="col-6">
+                <ul class="fa-ul ul-inline no-mar pull-right">
+                    <li class="top-apply header-button"><a href="/applications"><span class="fa-li"></span><i class="fal fa-file-alt"></i>Apply to Southern</a></li>
+                    <li class="top-donate header-button"><a href="/donate"><span class="fa-li"></span><i class="fas fa-gift"></i>Donate</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="header-bottom wrap-bg sec row">
+            <div class="col-3">
+      		    <a href="/"><img class="logo" src="/assets/images/southern_logo_white.png" alt="Southern West Virginia Community and Technical College"></a>
+            </div>
+            <div class="col-9 flex flex-right">
+                <nav class="nav nav-inline pull-right">
+                    <ul>
+                        <li><a href="/new-students">New Students</a></li>
+                        <li><a href="/student-services">Current Students</a></li>
+                        <li><a href="/online-learning">Online Class</a></li>
+                        <li><a href="/workforce-development">Workforce Development</a></li>
+                    </ul>            
+                </nav>
+            </div>
+        </div>
+  	</header>
+
+  	<main>
+	  	
+        <section class="sliders slidey wrap-bg no-pad-y">
+	  		<div class="slider centered" style="background-image: url(/assets/images/slider-counseling.jpg); background-position-y: 0%">
+                <div class="slider-text wrap on-topper">
+                    <h1 class="white-text">Have You Registered Yet?</h1>
+                    <h2 class="white-text superlight upper">Spring Registration Now Open</h2>
+                    <button class="ter superlight"><a class="white-text white-hover-text block-link" href="https://rand.wvnet.edu:9790/SCCPROD/twbkwbis.P_ValLogin" target="_BLANK">Register Now</a></button>
+                </div>   
+            </div>
+	  		<i class="slider-arrow slider-arrow-left on-topper trans pointer fal fa-angle-left"></i>
+	  		<i class="slider-arrow slider-arrow-right on-topper trans pointer fal fa-angle-right"></i>
+            <div class="overlay"></div>
+	  	</section>
+
+        <section class="home-splash wrap-bg bg-img centered rel" style="background-image: url(/assets/images/campus.jpg); background-position-y: 0%;">
+            <div class="on-topper rel">
+                <div class="home-splash-boxes row">
+                    <div class="col-4">
+                        <div class="home-splash-box bg-img center rel" style="background-image: url(/assets/images/programs.jpg);">
+                            <a class="block-link" href="/programs">
+                                <strong class="light white-text">Programs</strong>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="home-splash-box bg-img center" style="background-image: url(/assets/images/mySouthern.jpg);">
+                            <a class="block-link" href="https://rand.wvnet.edu:9790/SCCPROD/twbkwbis.P_WWWLogin" target="_BLANK">
+                                <strong class="light white-text">mySouthern</strong>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="home-splash-box bg-img center" style="background-image: url(/assets/images/scholarships.jpg);">
+                            <a class="block-link" href="/foundation/scholarships">
+                                <strong class="light white-text">Scholarships</strong>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <h1 class="tight thin-underline">Southern WV Community and Technical College</h1>
+                <p class="superlight p-tight">Southern West Virginia Community College was established as an independently accredited, comprehensive community college on July 1, 1971, through the consolidation of two existing branches of Marshall University.  These branches, located in Mingo and Logan Counties, had been in operation under Marshall’s direction since 1963 and provided the first two years of liberal arts and teacher education, as well as career programs in secretarial science and radiologic technology.  In 1971 these two locations became the first two campuses of Southern when it became an independently accredited institution of higher education.  It was also in 1971 that Southern’s first building was completed in Williamson.</p>
+                <p class="p-more-top"><a class="light upper" href="/about/history">Learn More<i class="fal fa-chevron-square-right fa-inline"></i></a></p>
+                <div class="home-splash-blurbs">
+                    <div class="row pad-middle">
+                        <div class="col-6 row">
+                            <i class="fas fa-shield-check"></i>
+                            <div class="home-splash-blurbs-text left">
+                                <h2 class="pri-text tight normal">Mission Statement</h2>
+                                <p class="p-small p-tight light">Southern West Virginia Community and Technical College provides accessible, affordable, quality education and training that promote success for those we serve. Southern aspires to establish itself as a model of leadership, academic excellence, collaboration, and occupational training, equipping its students with the tools necessary to compete and prosper in the regional and global economies of the twenty-first century.</p>
+                            </div>
+                        </div>
+                        <div class="col-6 row">
+                            <i class="fas fa-file-certificate"></i>
+                            <div class="home-splash-blurbs-text left">
+                                <h2 class="pri-text tight normal">eTranscripts</h2>
+                                <p class="p-small p-tight light">Transcript Ordering allows you to order official transcripts via the Web at any time, 24/7. Official transcripts can be delivered electronically in as little as 15 minutes from when you place your order! You can order as many transcripts as you like in one online session using any major credit card. Realtime email and mobile text alerts keep you up to date on the status of your order. You can also track your order on the Transcript Ordering Web site.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row pad-middle">
+                        <div class="col-6 row">
+                            <i class="fas fa-clock"></i>
+                            <div class="home-splash-blurbs-text left">
+                                <h2 class="pri-text normal">Class Schedules</h2>
+                                <p class="p-small p-tight light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                            </div>
+                        </div>
+                        <div class="col-6 row">
+                            <i class="fas fa-cogs"></i>
+                            <div class="home-splash-blurbs-text left">
+                                <h2 class="pri-text tight normal">Student Services</h2>
+                                <p class="p-small p-tight light">Student Support Services strive to assist students who want to improve their lives through education. People from all walks of life enroll at Southern, and we are committed to helping each student graduate. Stop by the Student Success Center at any of our campuses to learn how we can help you succeed at Southern.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="overlay"></div>
+        </section>
+
+        <section class="wrap">
+            <h1 class="tight thin-underline left">Latest News</h1>
+            <div class="row pad-middle">
+                 <div class="col-4">
+                    <h2>Southern West Virginia Community and Technical College’s Logan Campus to Host Regional West Virginia Math Field Day</h2>
+                    <p>The Logan campus of Southern West Virginia Community and Technical College will be hosting the Regional West Virginia Math Field Day Friday, February 8, 2019. The competition will begin in Building A...</p>
+                </div>
+                <div class="col-4">
+                    <h2>Southern West Virginia Community and Technical College to Host Tax Prep Events in Logan, Williamson</h2>
+                    <p>Southern West Virginia Community and Technical College has partnered with United Way of Central West Virginia to offer free tax preparation services to individuals and families through the IRS-...</p>
+                </div>
+                <div class="col-4">
+                    <h2>Notice of Open Meeting</h2>
+                    <p>The Southern West Virginia Community and Technical College Board of Governors Agenda Committee will conduct a meeting on Tuesday, February 5, 2019, beginning at 9:00 a.m. at Southern West Virginia...</p>
+                </div>
+            </div>
+        </section>
+
+        <section class="wrap">
+            <h1 class="centered">Join the Coon Mailing List</h1>
+            <form action="/sub.php">
+                <label for="name">Name</label>
+                <input name="name" id="name" type="text">
+                
+                <label for="email">E-Mail</label>
+                <input name="email" id="email" type="text">
+
+                <label for="food">Favorite Dumpster Food</label>
+                <select name="food" id="food">
+                    <option value="" disabled selected>---Select A Food---</option>
+                    <option value="">Banana Peels</option>
+                    <option value="">Apple Cores</option>
+                    <option value="">Cat Food</option>
+                    <option value="">Vegetables</option>
+                    <option value="">Raw Meat</option>
+                </select>
+
+                <label for="more">Tell Us More About Yourself</label>
+                <textarea name="more" id="more"></textarea>
+
+                <button class="btn" type="submit">Sign Up Now!</button>
+            </form>
+        </section>
+
+  	</main>
+
+  	<footer class="footer wrap-bg centered">
+  		<p class="no-mar-y sec-text">&copy; <?php echo date('Y'); ?> Property of Coon</p>
+  	</footer>
+
+  	<script src="/assets/js/script.js"></script>
+
+  </body>
+
+</html>
